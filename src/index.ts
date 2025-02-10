19e6d28d0e0740e1758875d2d3ffc315bfccddee
DominiceLanzarini <dlanzarini@gmail.com>
2025-02-10 15:07:06 -0300

    Branch 1 Done

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
-import { AppDataSource } from "./data-source"
-import express from "express"
-import cors from "cors"
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+import { AppDataSource } from "./data-source";
+import express from "express";
+import cors from "cors";
 
-import userRouter from "./routes/user.routes"
-import authRouter from "./routes/auth.routes"
-import medicamentosRouter from "./routes/medicamentos.routes"
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+import userRouter from "./routes/user.routes";
+import authRouter from "./routes/auth.routes";
+import medicamentosRouter from "./routes/medicamentos.routes";
 
-app.use("/users", userRouter)
-app.use("/login", authRouter)
-app.use("/medicamentos", medicamentosRouter)
+import authenticate from "./middleware/authenticate"
 
-AppDataSource.initialize().then(async () => {
+app.use(cors());
+app.use(express.json());
 
+app.use("/users", authenticate, userRouter);
+app.use("/login", authRouter);
+app.use("/medicamentos", medicamentosRouter);
+
+AppDataSource.initialize()
+  .then(async () => {
     app.listen(3000, () => {
-        console.log("Servidor rodando na porta http://localhost:3000")
-    })
-
-}).catch(error => console.log(error))
+      console.log("Servidor rodando na porta http://localhost:3000");
+    });
+  })
+  .catch((error) => console.log(error));
